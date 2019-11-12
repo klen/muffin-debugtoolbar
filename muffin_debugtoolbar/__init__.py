bce845f48ae3f21c859663190f5be3ae69970ccc
Kirill Klenov <horneds@gmail.com>
2019-11-12 19:50:00 +0300

    Bump version: 0.3.0 → 0.3.1

--- muffin_debugtoolbar/__init__.py
+++ muffin_debugtoolbar/__init__.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __project__ = "muffin-debugtoolbar"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
